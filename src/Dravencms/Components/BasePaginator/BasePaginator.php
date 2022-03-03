ef2219a43a13d36e0fe564253fd3472105500628
Adam Schubert <adam.schubert@sg1-game.net>
2022-03-03 23:30:26 +0100

    chore: Save

--- src/Dravencms/Components/BasePaginator/BasePaginator.php
+++ src/Dravencms/Components/BasePaginator/BasePaginator.php
@@ -10,7 +10,7 @@ use Nette\Localization\ITranslator;
 /**
  * Copyright (C) 2016 Adam Schubert <adam.schubert@sg1-game.net>.
  */
-class BasePaginator extends BaseControl implements BasePaginatorFactory
+class BasePaginator extends BaseControl
 {
     /** @var ITranslator */
     private $translator;
@@ -20,17 +20,26 @@ class BasePaginator extends BaseControl implements BasePaginatorFactory
         $this->translator = $translator;
     }
 
+    public function render(): void
+    {
+        $template = $this->template;
+        $template->activeLocales = $this->localeRepository->getActive();
+        $template->fileSelectorPath = $this->fileFile->getFileSelectorPath();
+        $template->setFile(__DIR__ . '/DownloadFileForm.latte');
+        $template->render();
+    }
+    
     /**
      * @param IContainer $container
      * @param $name
      * @return Grid
      */
-    public function create(IContainer $container = null, string $name = null)
+    /*public function create(IContainer $container = null, string $name = null)
     {
         $control = new Control();
         $control->setTemplateFile(__DIR__.'/bootstrap-localized.latte');
 
         return $control;
-    }
+    }*/
 
 }
--- src/Dravencms/Paginator/DI/PaginatorExtension.php
+++ src/Dravencms/Paginator/DI/PaginatorExtension.php
@@ -11,8 +11,27 @@ use Nette\DI\CompilerExtension;
  */
 class PaginatorExtension extends CompilerExtension implements TranslationProviderInterface
 {
+    public function loadConfiguration(): void
+    {
+        $this->loadComponents();
+    }
+    
+    protected function loadComponents(): void
+    {
+        $builder = $this->getContainerBuilder();
+        foreach ($this->loadFromFile(__DIR__ . '/components.neon') as $i => $command) {
+            $cli = $builder->addFactoryDefinition($this->prefix('components.' . $i));
+            if (is_string($command)) {
+                $cli->setImplement($command);
+            } else {
+                throw new \InvalidArgumentException;
+            }
+        }
+    }
+    
     public function getTranslationResources(): array
     {
         return [__DIR__.'/../lang'];
     }
+    
 }
